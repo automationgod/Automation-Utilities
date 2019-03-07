45b933966529627bf388213dc8274a464f5358b8
automationgod <automationgithubgit@gmail.com>
2019-03-07 23:37:31 +0530

    Upload Utilities for automation test data management

---
+++ AutomationTestDataManagementUtilities/CSVUtilities.java
@@ -0,0 +1,43 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.regex.Pattern;
+
+
+public class CSVUtilities {
+
+	public Map<String, String> getCSVDataAsMAP(String strCSVFilePath){
+		
+		File file = new File(strCSVFilePath);
+		ArrayList<String> oData = new ArrayList<String>();
+		Scanner scan = null;
+		Map<String, String> oMap = new HashMap<String, String>();
+		try {
+			scan = new Scanner(file);
+		} catch (Exception e) {
+			System.out.println("File not found in derectory - "+strCSVFilePath);
+		}
+		String line = null;
+		while(scan.hasNextLine()){
+			line = scan.nextLine();
+			if(line!=null){
+				oData.add(line);
+			}
+		}
+		String oHeader = oData.get(0);
+		ArrayList<String> values = new ArrayList<String>();
+		for(int i=1; i<oData.size();i++){
+			values.add(oData.get(i));
+		}
+		
+		String[] headerArray = oHeader.split(Pattern.quote(","));
+		String[] arrayValue = values.get(0).split(Pattern.quote(","));
+		for(int j=0;j<headerArray.length; j++){
+			oMap.put(headerArray[j], arrayValue[j]);
+		}
+		
+		return oMap;
+	}
+}
